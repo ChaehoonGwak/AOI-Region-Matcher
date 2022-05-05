372badb46616f9a4c4568a5502b2e1dbcf513b40
Chaehoon <chaehoon.gwak@gmail.com>
2022-05-06 00:53:14 +0900

    connect postgre DB

---
+++ src/main/java/com/sia/assignment/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.sia.assignment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String getHome( ){
+        return "hello";
+    }
+}
