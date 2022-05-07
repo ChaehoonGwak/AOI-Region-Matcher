0b46cffac5cab1158f587af0b264cf33def5a9ab
Chaehoon <chaehoon.gwak@gmail.com>
2022-05-07 17:57:33 +0900

    Delete PointEntity, PolygonEntity

--- src/main/java/com/sia/assignment/repository/PointEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sia.assignment.repository;
-
-import com.sia.assignment.domain.PointEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PointEntityRepository extends JpaRepository<PointEntity, Long> {
-}
--- src/main/java/com/sia/assignment/repository/PolygonEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sia.assignment.repository;
-
-import com.sia.assignment.domain.PolygonEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PolygonEntityRepository extends JpaRepository<PolygonEntity, Long> {
-}
